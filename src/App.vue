98913e798d125fea0fa11b414aabeab72aab90f1
KitBryan <bryanwwk@gmail.com>
2024-03-18 19:49:40 +0800

    Chart update works

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from "vue";
+import LineChart from "@/components/LineChart.vue";
+
+let chartValues = ref({
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+        {
+            label: 'Data One',
+            data: [40, 39, 10, 40, 39, 80, 40]
+        }
+    ]
+})
+
+let myDatasets = [
+    [
+        {
+            label: 'dummy-temp-1',
+            data: [20, 20, 20, 20, 20, 20, 20]
+        }
+    ], [
+        {
+            label: 'dummy-temp-2',
+            data: [20, 20, 30, 20, 20, 40, 20]
+        }
+    ], [
+        {
+            label: 'dummy-temp-3',
+            data: [20, 60, 20, 80, 20, 10, 20]
+        }
+    ],
+]
+</script>
+
+<template>
+    <LineChart :chart-data="chartValues" :chart-options=" {responsive: true,maintainAspectRatio: true}"/>
+    <!-- TODO: Continue this-->
+    <button @click="()=>{
+        const randomNumber = Math.random()
+        chartValues = {...chartValues, datasets: myDatasets[Math.floor(Math.random()*3)]
+        }
+    }
+"> click me
+    </button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/LineChart.vue
@@ -0,0 +1,35 @@
+<template>
+    <Line
+        :data="chartData"
+        :options="chartOptions"
+    />
+    {{chartData}}
+</template>
+
+<script>
+
+import { Line } from 'vue-chartjs'
+import {
+    Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend
+} from 'chart.js'
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
+
+export default {
+    props: ['chartData', 'chartOptions'],
+    name: 'LineChart',
+    components: {Line},
+    computed: {
+        updateChartData : function () {
+            return this.chartData;
+        },
+        updateChartOptions() {
+            return this.chartOptions;
+        }
+    }
+}
+</script>
+
+
+
+
